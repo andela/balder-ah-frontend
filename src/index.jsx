84509722642dfb73a061a11e4829844abf395da6
Kizito Akhilome <kizito@akhilo.me>
2019-01-09 11:38:20 +0100

    [Chore #162170661] Setup redux (#4)
    - install redux, react-redux, and redux-thunk
    - configure app to use redux and setup placeholder reducer
    - create store and setup redux folder structure
    - setup redux dev tools
    
    [Finishes #162170661]

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
