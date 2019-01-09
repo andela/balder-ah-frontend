5c7ea911574629c25c1f80f8dc51ac22a159be01
Oluwakunle Fakorede <40468678+frostyblok@users.noreply.github.com>
2019-01-09 08:49:43 +0100

    [feature #162170665] Setup React router (#2)
    - install react-router-dom
    - create home and login components
    [finishes #162170665]

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        { 'Welcome to Author\'s Haven by Team Balder'}
-      </p>
-    </header>
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+    </Switch>
+  </Router>
 );
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>
+        { 'Welcome to Author\'s Haven by Team Balder'}
+      </p>
+    </header>
+  </div>
+);
+
+export default Home;
